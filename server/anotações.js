7b7ab1b5f1b6e32a01b755668642e1f327b58949
Jonas Araujo <jonassilvadearaujo18@gmail.com>
2020-07-20 22:19:04 -0300

    App

---
+++ server/anotações.js
@@ -0,0 +1,17 @@
+//Rota: Endereço completo da requisição, é bem semântico Ex: localhost:3333/users
+//Recurso: Qual entidade do sistema estamos acessando Ex: /users
+
+//**** Metódos HTTP ********* //
+
+//GET: Buscar uma ou mais informações do back-end
+//POST: Criar uma nova informação no back-end
+//PUT: Atualizar uma informação existente no back-end
+//DELETE: Remover uma informação existente no back-end
+
+//**** Tipos de parâmetro ********* //
+
+//Request Param: Parâmetros que vem na própia rota que identificam um recurso específico
+//Query Param: Geralmente opcionais, servem pra filtrar, paginação, na própia rota também
+//Request Body: Parâmetros para criação / atualização de informação
+
+//Migrations: Histórico do banco de dados
